b4541da2dd3f91e51354bba77e1fc3ca3dbb5d0b
ncontos522 <ncontos24@jcu.edu>
2022-03-31 15:14:28 -0400

    hello

--- jUnitTestJNPD/src/jUnitTestJNPD/Tests.java
+++ jUnitTestJNPD/src/jUnitTestJNPD/Tests.java
@@ -11,7 +11,7 @@ public class Tests {
 
 	testQuestion question;
 	capsCheck capsCheck; // sara was here
-  
+
 	/**
 	 * Gives a test question to the user and asks the user to answer If the user
 	 * answers correctly, returns true. else, false.
@@ -48,7 +48,7 @@ public class Tests {
 	 */
 	@Test
 	public void happyCheckCapitalization() {
-		String para = "Hello my name is Muhammadu Buhari and I am the president of Nigeria. I have been president for 13 years now. I like to stream fortnite on discord.";
+		String para = "Hello my name  is Muhammadu Buhari and I am the president of Nigeria. I have been president for 13 years now. I like to stream fortnite on discord.";
 		boolean isProperPara = capsCheck.properCaps(para);
 		assertEquals(isProperPara, true);
 	}
@@ -63,6 +63,7 @@ public class Tests {
 		String para = "Hello my name is Muhammadu Buhari and i am the president of Nigeria. I have been president for 13 years now. my favorite thing to do is to stream fortnite on discord.";
 		boolean isProperPara = capsCheck.properCaps(para);
 		assertEquals(isProperPara, false);
+		System.out.println("Hello");
 	}
 
 }
