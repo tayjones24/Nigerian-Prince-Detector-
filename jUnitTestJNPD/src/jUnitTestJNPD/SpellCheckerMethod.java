1514d3742b9c50ec373873041d8493628cbdeff1
sconway24 <99760109+sconway24@users.noreply.github.com>
2022-04-07 08:21:13 -0400

    Attempt of a Spell checker

---
+++ jUnitTestJNPD/src/jUnitTestJNPD/SpellCheckerMethod.java
@@ -0,0 +1,10 @@
+
+public class SpellCheckerMethod {
+	int count =0;
+	
+    public void isSpelledCorrectly(String email) {
+    	for(int i<0;i<email.length();i++) {
+    		email[i] = SpellChecker.createCheckerPopup(sco);
+    	}
+    }
+}
